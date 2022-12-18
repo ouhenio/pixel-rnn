a1626dd76bf41b6cbdce5e331a9146bbc7ad54be
Eugenio Herrera <eiherrera@uc.cl>
2022-12-17 21:46:31 -0300

    Define general model classes

---
+++ pixel-rnn/model.py
@@ -0,0 +1,43 @@
+import torch
+
+class MaskedConvA(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class MaskedConvB(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class RowLSTM(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class DiagonalBiLSTM(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class PixelRNN(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+class PixelCNN(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x):
+        pass
