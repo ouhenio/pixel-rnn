7487f90dd8f3aca5f2cdb25111d8a83348dd559a
Eugenio Herrera <eiherrera@uc.cl>
2022-12-17 21:35:49 -0300

    Fix dataloader validator and improve syntax

--- pixel-rnn/data.py
+++ pixel-rnn/data.py
@@ -3,8 +3,16 @@ from torchvision import datasets, transforms
 
 import pytorch_lightning as pl
 
+BASE_DATA_DIR = "./data"
+
 class CIFAR10DataModule(pl.LightningDataModule):
-    def __init__(self, train_transform, data_dir='./', batch_size=32, num_workers=8):
+    def __init__(
+            self,
+            train_transform: transforms,
+            data_dir: str = BASE_DATA_DIR,
+            batch_size: int = 32,
+            num_workers: int = 8,
+        ):
         super().__init__()
         self.train_transform = train_transform
         self.val_transform = transforms.ToTensor()
@@ -16,17 +24,32 @@ class CIFAR10DataModule(pl.LightningDataModule):
         datasets.CIFAR10(root=self.data_dir, train=True, download=True)
         datasets.CIFAR10(root=self.data_dir, train=False, download=True)
     
-    def setup(self, stage=None):
+    def setup(self, stage: str = None):
         if stage == 'fit' or stage is None:
-            self.train_dset = datasets.CIFAR10(root=self.data_dir, train=True,
-                                               transform=self.train_transform)
-            self.val_dset = datasets.CIFAR10(root=self.data_dir, train=False,
-                                             transform=self.val_transform)
+            self.train_dset = datasets.CIFAR10(
+                root=self.data_dir,
+                train=True,
+                transform=self.train_transform
+            )
+            self.val_dset = datasets.CIFAR10(
+                root=self.data_dir,
+                rain=False,
+                transform=self.val_transform
+            )
     
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dset, batch_size=self.batch_size,
-                                           num_workers=self.num_workers, pin_memory=True)
+        return torch.utils.data.DataLoader(
+            self.train_dset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
 
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dset, batch_size=self.batch_size,
+        return torch.utils.data.DataLoader(
+            self.val_dset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True
+        )
